b6b42d5445387638c48d148c4d41caccb7ee8df0
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-10-02 23:56:12 +0000

    automatically backed up by learn

--- spec/01_greeting_spec.rb
+++ spec/01_greeting_spec.rb
@@ -17,7 +17,7 @@ describe './lib/greeting.rb' do
 
       output = capture_puts{ greeting("Sally") }
 
-      expect(output).to include("Hello Sally. It's nice to meet you.")
+      expect(output).to include("Hello Sally. It's nice to meet you.")le
     end
   end
 end
